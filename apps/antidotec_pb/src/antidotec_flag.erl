fd74e44a2197a0b89c8dd0d86ae5c86228ee4678
v0idpwn <v0idpwn@gmail.com>
2022-06-06 16:45:50 +0300

    Add support for flags and maps on protobuf client
    Also fixes a bug in registers where no operations would be sent.

--- apps/antidotec_pb/src/antidotec_flag.erl
+++ apps/antidotec_pb/src/antidotec_flag.erl
@@ -17,7 +17,7 @@
 %% under the License.
 %%
 %% -------------------------------------------------------------------
--module(antidotec_reg).
+-module(antidotec_flag).
 
 -include_lib("antidote_pb_codec/include/antidote_pb.hrl").
 
@@ -32,54 +32,64 @@
          type/0
         ]).
 
--export([assign/2
-        ]).
+-export([assign/2, enable/1, disable/1]).
 
--record(antidote_reg, {
+-record(antidote_flag, {
             value,
             new_value
          }).
 
--export_type([antidote_reg/0]).
--opaque antidote_reg() :: #antidote_reg{}.
+-export_type([antidote_flag/0]).
+-opaque antidote_flag() :: #antidote_flag{}.
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
 
--spec new() -> antidote_reg().
+-spec new() -> antidote_flag().
 new() ->
-    #antidote_reg{value=""}.
+    #antidote_flag{value=false}.
 
 
 new(Value) ->
-    #antidote_reg{value=Value}.
+    #antidote_flag{value=Value}.
 
--spec value(antidote_reg()) -> [term()].
-value(#antidote_reg{value=Value}) -> Value.
+-spec value(antidote_flag()) -> [term()].
+value(#antidote_flag{value=Value}) -> Value.
 
-dirty_value(#antidote_reg{value=Value, new_value=undefined}) -> Value;
-dirty_value(#antidote_reg{new_value=NewValue}) -> NewValue.
+dirty_value(#antidote_flag{value=Value, new_value=undefined}) -> Value;
+dirty_value(#antidote_flag{new_value=NewValue}) -> NewValue.
 
 %% @doc Determines whether the passed term is a reg container.
 -spec is_type(term()) -> boolean().
 is_type(T) ->
-    is_record(T, antidote_reg).
+    is_record(T, antidote_flag).
 
 %% @doc Returns the symbolic name of this container.
--spec type() -> reg.
-type() -> reg.
+-spec type() -> flag.
+type() -> flag.
 
-to_ops(_, #antidote_reg{new_value=undefined}) ->
-  [];
+to_ops(_, #antidote_flag{new_value=undefined}) ->
+    [];
 
-to_ops(BoundObject, #antidote_reg{new_value=NewValue}) ->
-    [{BoundObject, assign, NewValue}].
+to_ops(BoundObject, #antidote_flag{new_value=true}) ->
+    [{BoundObject, enable, {}}];
 
-assign(Reg, Value) ->
-    Reg#antidote_reg{new_value=Value}.
+to_ops(BoundObject, #antidote_flag{new_value=false}) ->
+    [{BoundObject, disable, {}}].
 
+-spec assign(antidote_flag(), boolean())  -> antidote_flag().
+assign(Flag, Value) ->
+    Flag#antidote_flag{new_value=Value}.
+
+-spec enable(antidote_flag())  -> antidote_flag().
+enable(Flag) ->
+    Flag#antidote_flag{new_value=true}.
+
+-spec disable(antidote_flag())  -> antidote_flag().
+disable(Flag) ->
+    Flag#antidote_flag{new_value=false}.
 
 %% ===================================================================
 %% EUnit tests
