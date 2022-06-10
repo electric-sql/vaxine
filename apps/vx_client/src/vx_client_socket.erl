5527a5dce7cec60fc9529de7f44b19c5f1106507
Defnull <define.null@gmail.com>
2022-06-10 11:56:47 +0200

    Backoff for client reconnects

--- apps/vx_client/src/vx_client_socket.erl
+++ apps/vx_client/src/vx_client_socket.erl
@@ -9,7 +9,8 @@
          terminate/2]).
 
 -define(CONN_TIMEOUT, 1000).
--define(RECONN_INTERVAL, 1000).
+-define(RECONN_MIN, 500).
+-define(RECONN_MAX, 5000). %% 5 seconds
 
 -type address() :: inet:socket_address() | inet:hostname().
 -type sub_id() :: vx_client:sub_id().
@@ -21,7 +22,7 @@
                          ],
                  socket :: gen_tcp:socket() | undefined,
                  connect_timeout = ?CONN_TIMEOUT :: non_neg_integer(),
-                 reconnect_interval = ?RECONN_INTERVAL :: non_neg_integer(),
+                 reconnect_backoff = backoff:backoff(),
                  reconnect_tref :: reference() | undefined
                }).
 
@@ -56,9 +57,13 @@ stop(Pid) ->
 %%------------------------------------------------------------------------------
 
 init([Address, Port, Options]) ->
+    Backoff0 = backoff:init(?RECONN_MIN, ?RECONN_MAX, self(), reconnect),
+    Backoff1 = backoff:type(Backoff0, jitter),
+
     State = #state{ address = Address,
                     port = Port,
-                    opts = Options
+                    opts = Options,
+                    reconnect_backoff = Backoff1
                   },
 
     case connect_int(Address, Port, Options, State) of
@@ -86,12 +91,15 @@ handle_info({tcp_error, _, Reason}, State) ->
 handle_info({tcp_closed, _}, State) ->
     maybe_reconnect(tcp_closed, State);
 handle_info({timeout, Tref, reconnect}, #state{reconnect_tref = Tref} = State) ->
+    Backoff = State#state.reconnect_backoff,
     case
         connect_int(State#state.address, State#state.port,
                      State#state.opts, State)
     of
         {ok, State1} ->
-            {noreply, State1};
+            {noreply, State1#state{reconnect_backoff = backoff:succeed(Backoff),
+                                   reconnect_tref = undefined
+                                  }};
         {error, Reason} ->
             maybe_reconnect(Reason, State)
     end;
@@ -102,10 +110,11 @@ terminate(_Reason, _State) -> ok.
 
 %%------------------------------------------------------------------------------
 
-maybe_reconnect(Error, State = #state{opts = Options}) ->
+maybe_reconnect(Error, State = #state{reconnect_backoff = Backoff0, opts = Options}) ->
     case proplists:get_bool(auto_reconnect, Options) of
         true ->
-            {noreply, schedule_reconnect(State)};
+            {_, Backoff1} = backoff:fail(Backoff0),
+            {noreply, schedule_reconnect(State#state{reconnect_backoff = Backoff1})};
         false ->
             {stop, Error}
     end.
@@ -128,6 +137,5 @@ disconnect_int(State = #state{socket = Socket}) ->
     catch gen_tcp:close(Socket),
     State#state{socket = undefined}.
 
-schedule_reconnect(State) ->
-    Tref = erlang:start_timer(State#state.reconnect_interval, self(), reconnect),
-    State#state{ reconnect_tref = Tref }.
+schedule_reconnect(State = #state{reconnect_backoff = Backoff}) ->
+    State#state{reconnect_tref = backoff:fire(Backoff)}.
